eda7a5978bce672e769f416cad20eb67c2fcc5c4
Kayque <kayquecarvalho020@gmail.com>
2024-10-03 11:25:25 -0300

    Criando função em javascript

--- script.js
+++ script.js
@@ -11,3 +11,26 @@ while (f <10){
     console.log(f)
     f++;
 }
+
+// do while - enquanto
+
+let w=0;
+
+do{
+    //console.log(w);
+    w++;
+}
+while(w<5);
+
+// função - um conjunto de dados para serem executados
+
+function ola(nome){
+    // ´´ (crase) string literal ou template
+    console.log(`Seja bem cindo a, ${nome}`)
+    alert(`seja bem vindo a, ${nome}`)
+    document.write(`seja bem vindo a, ${nome}`)
+}
+//Executando a função ola passando valor
+ola("TESTE")
+
+
