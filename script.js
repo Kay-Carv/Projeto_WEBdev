95c9d493755ec259afdf4ac22d6b333d024eae72
Kayque <kayquecarvalho020@gmail.com>
2024-10-03 11:12:28 -0300

    estrutura usando for e while

---
+++ script.js
@@ -0,0 +1,13 @@
+//Estrutura de repetição
+                            //declarei o i=0
+for (let i=0; i<10; i++){   //i++ vai acrecentar +1
+    console.log(i)
+}
+
+//While
+
+f =0;
+while (f <10){
+    console.log(f)
+    f++;
+}
